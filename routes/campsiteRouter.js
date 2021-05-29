3fad4f9b429ee6cfd9f6ec9d8891860689d1066d
Sean Browne <sbrowne15@gmail.com>
2021-05-29 11:37:15 -0500

    Update Week 3 Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -184,7 +184,7 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
                 })
                 .catch(err => next(err));
             } else {
-                err = new Error(`You are not authorized to update comment.`);
+                err = new Error(`You are not authorized to update this comment.`);
                 err.status = 403;
                 return next(err);
             }
@@ -204,14 +204,20 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
-            campsite.comments.id(req.params.commentId).remove();
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
+            if((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id))  {
+                campsite.comments.id(req.params.commentId).remove();
+                campsite.save()
+                .then(campsite => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(campsite);
+                })
+                .catch(err => next(err));
+            } else {
+                err = new Error(`You are not authorized to delete this comment.`);
+                err.status = 403;
+                return next(err);
+            }
         } else if (!campsite) {
             err = new Error(`Campsite ${req.params.campsiteId} not found`);
             err.status = 404;
